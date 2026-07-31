f5439439595271887aa86a33389d879dded480d0
Soumitra Das <125145966+CodeBySoumitra@users.noreply.github.com>
2026-07-31 14:07:33 +0530

    Delete array/input.java

--- array/input.java
+++
@@ -1,8 +0,0 @@
-public static void inputArr(int[] arr){
-        Scanner sc = new Scanner(System.in);
-        int n = arr.length;
-        System.out.print("Enter "+n+" array elements: ");
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = sc.nextInt();
-        }
-    }
